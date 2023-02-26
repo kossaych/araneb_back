fbbf98285feba84890f512baef228fc864523e0d
kossaych <cheminguikossay@gmail.com>
2023-02-26 09:51:06 -0800

    first commit

--- lapinproduction/models.py
+++ lapinproduction/models.py
@@ -5,7 +5,7 @@ from accounts.models import User
 from django.db.models.signals import post_save
 from django.utils import timezone
 from datetime import datetime,date
-import pandas as pd 
+#import pandas as pd 
 from accounts.models import Race
 # calculer l'age d'un lapin retourner le nombre de jour a partir du date de naissance (str!!) il retourne un entier
 def age(naissance):
